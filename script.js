fea758422585c62eb8c2b0a7956f816624bb4ed2
purkpine <112010018+purkpine@users.noreply.github.com>
2026-02-08 16:56:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const title = document.getElementById("letter-title");
 const catImg = document.getElementById("letter-cat");
 const buttons = document.getElementById("letter-buttons");
 const finalText = document.getElementById("final-text");
-const contador = 1 
+const contador = 0
 // Click Envelope
 
 envelope.addEventListener("click", () => {
@@ -22,27 +22,27 @@ envelope.addEventListener("click", () => {
 
 // Logic to move the NO btn
 
-//noBtn.addEventListener("mouseover", () => {
-//   const min = 200;
-//    const max = 200;
-//
-//    const distance = Math.random() * (max - min) + min;
-//    const angle = Math.random() * Math.PI * 2;
-//
-//    const moveX = Math.cos(angle) * distance;
-//    const moveY = Math.sin(angle) * distance;
-//
-//    noBtn.style.transition = "transform 0.3s ease";
-//    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+noBtn.addEventListener("click", () => {
+   const min = 200;
+    const max = 200;
+
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
+
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
 //});
 
 // Logic to make YES btn to grow
 
  let yesScale = 1;
-
- yesBtn.style.position = "relative"
- yesBtn.style.transformOrigin = "center center";
- yesBtn.style.transition = "transform 0.3s ease";
+ Let contador =1;
+ //yesBtn.style.position = "relative"
+ //yesBtn.style.transformOrigin = "center center";
+// yesBtn.style.transition = "transform 0.3s ease";
 
  noBtn.addEventListener("click", () => {
     
@@ -71,7 +71,7 @@ case 6:
   default:
     title.textContent = "wololo";
 }
-contador +=1
+contador +=1;
      if (yesBtn.style.position !== "fixed") {
          yesBtn.style.position = "fixed";
          yesBtn.style.top = "50%";
