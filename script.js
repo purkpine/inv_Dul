6671ea6bebf92263175418d9402a9ab3b5fa3a04
purkpine <112010018+purkpine@users.noreply.github.com>
2026-02-08 22:09:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,37 +45,37 @@ envelope.addEventListener("click", () => {
  yesBtn.style.transition = "transform 0.3s ease";
 
  noBtn.addEventListener("click", () => {
-         switch (contador) {
-     case 2:{
-        title.textContent = "jeje creo que le picaste mal";
-        catImg.src = "cat_kitty.gif";
-         break;
-  }
-    case 3:{
-        title.textContent = "segura?!!";
-        catImg.src = "cat_gun.gif";
-         break;
-  }
-    case 4:{
-        title.textContent = "es neta?!!";
-        catImg.src = "cat_sad.gif";
-         break;
-  }
-    case 5:{
-        title.textContent = "andale, ya bien";
-        catImg.src = "lloro.gif";
-         break;
-  }
-    case 6:{
-        title.textContent = "segura?!!";
-        catImg.src = "cat_gun.gif";
-         break;
-  }
-    default:
-        ;
-};
+ //         switch (contador) {
+ //     case 2:{
+ //        title.textContent = "jeje creo que le picaste mal";
+ //        catImg.src = "cat_kitty.gif";
+ //         break;
+ //  }
+ //    case 3:{
+ //        title.textContent = "segura?!!";
+    //     catImg.src = "cat_gun.gif";
+       //   break;
+//   }
+   //  case 4:{
+      //   title.textContent = "es neta?!!";
+//         catImg.src = "cat_sad.gif";
+//          break;
+//   }
+//     case 5:{
+//         title.textContent = "andale, ya bien";
+ //        catImg.src = "lloro.gif";
+//          break;
+//   }
+//     case 6:{
+//         title.textContent = "segura?!!";
+ //        catImg.src = "cat_gun.gif";
+ //         break;
+ //  }
+ //    default:
+ //        ;
+// };
      yesScale += 1;
-     contador +=1;
+//      contador +=1;
 
      if (yesBtn.style.position !== "fixed") {
          yesBtn.style.position = "fixed";
